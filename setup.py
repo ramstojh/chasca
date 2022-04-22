5e900c766bcdd20242152e474175f8c2056b0f33
Jhon Yana <ramstojh@gmail.com>
2022-04-22 16:24:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='chasca',
       version='0.1',
-      description='Tools for planning astronomical observations',
+      description='Tools to help people to plan astronomical observations',
       url='https://github.com/ramstojh/chasca',
       author='Jhon Yana',
       author_email='ramstojh@alumni.usp.br',
